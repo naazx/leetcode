04a0d2d74be2b76de5c9ca3d0cb97c9ce250683c
Nazar Dydyn <nazardydyn83@gmail.com>
2026-09-02 20:09:02 +0300

    Time: 0 ms (100%), Space: 25.8 MB (96.07%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.swift
+++ 0739-daily-temperatures/0739-daily-temperatures.swift
@@ -1,6 +1,26 @@
 class Solution {
     func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
         var res = [Int](repeating: 0, count: temperatures.count)
+        var stack: [Int] = []
+        let n = temperatures.count
+        
+        for i in 0..<n {
+            while let lastIndex = stack.last, temperatures[i] > temperatures[lastIndex] {
+                let prevIndex = stack.removeLast()
+                res[prevIndex] = i - prevIndex
+            }
+            stack.append(i)
+        }
+        
+        return res
+    }
+}
+
+
+/*
+class Solution {
+    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
+        var res = [Int](repeating: 0, count: temperatures.count)
         
         let n = temperatures.count
         for i in 0..<n {
@@ -14,4 +34,4 @@ class Solution {
         
         return res
     }
-}
+} */
