4673478079bfca60020f77b37b7819240360ef2f
Nazar Dydyn <nazardydyn83@gmail.com>
2026-09-22 18:23:37 +0300

    Time: 26 ms (16.95%), Space: 19.6 MB (72.88%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.swift
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSubtree(_ root: TreeNode?, _ subRoot: TreeNode?) -> Bool {
+        if subRoot == nil {
+             return true 
+             }
+        guard let root = root else { return false }
+        
+        if isSameTree(root, subRoot) {
+            return true
+        }
+        
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    }
+    
+    private func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil { 
+            return true 
+            }
+        guard let p = p, let q = q else { 
+            return false 
+            }
+        
+        return p.val == q.val 
+            && isSameTree(p.left, q.left) 
+            && isSameTree(p.right, q.right)
+    }
+}
+
